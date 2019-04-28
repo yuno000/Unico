f91dc02ba17cebe1965550ffd4b8cb93f6fe3f44
yuno000 <yuukabis77@gmail.com>
2019-04-28 17:33:24 +0900

    マルチタップ実装

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameScripts : MonoBehaviour {
+public class EnemyScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
