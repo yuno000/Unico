7a2b829e12ac3b964de6bf99ccb54eb0a36b1f71
yuno000 <yuukabis77@gmail.com>
2019-04-28 20:02:53 +0900

    判定の丸の追加の途中

---
+++ Assets/Script/OsakanaScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OsakanaScript : MonoBehaviour {
+
+    public int osakanaHP = 1;
+    public int osakanaCount;
+    public GameObject osakana;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Damageosakana()
+    {
+        osakanaHP--;
+        if(osakanaHP <= 0)
+        {
+            Destroy(osakana);
+            osakanaCount++;
+
+        }
+    }
+}
