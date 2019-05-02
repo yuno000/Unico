4bd0146e945be9adf043f036659378ee6b0faead
yuno000 <yuukabis77@gmail.com>
2019-05-03 00:41:55 +0900

    タップの判定をしたいけどうまくいかない

---
+++ Assets/Script/TogeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchScript.Gestures;
+
+public class TogeScript : MonoBehaviour {
+    public GameObject[] toge;
+    
+    void HandleTapped(object sender, System.EventArgs e)
+    {
+        toge[0].transform.localScale = new Vector2(0, 40);
+        Debug.Log("tap"); 
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
