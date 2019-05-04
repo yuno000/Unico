821311a6027c40124d86cab2d1731de4196bfa27
yuno000 <yuukabis77@gmail.com>
2019-05-04 15:49:53 +0900

    tap判定途中

--- Assets/Script/TogeScript.cs
+++ Assets/Script/TogeScript.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using TouchScript.Gestures;
 
 public class TogeScript : MonoBehaviour {
     public GameObject[] toge;
-    
-    void HandleTapped(object sender, System.EventArgs e)
+    public TapGesture tapGesture;
+
+
+    private void OnEnable()
     {
-        toge[0].transform.localScale = new Vector2(0, 40);
-        Debug.Log("tap"); 
+
+        tapGesture.Tapped += OnTapped;
     }
+
+    private void OnDisable()
+    {
+        tapGesture.Tapped -= OnTapped;
+    }
+
+    private void OnTapped(object sender, EventArgs e)
+    {
+        Debug.Log("タップされた");
+    }
+
+
+    //void HandleTapped(object sender, System.EventArgs e)
+    //{
+    //    toge[0].transform.localScale = new Vector2(0, 40);
+    //    Debug.Log("tap"); 
+    //}
 
     // Use this for initialization
     void Start () {
